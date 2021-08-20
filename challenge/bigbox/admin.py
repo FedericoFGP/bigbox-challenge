5f39ef962a8ecfd22005b724af5b4956c568cdf3
Federico Plastina <40448913+FedericoFGP@users.noreply.github.com>
2021-08-20 09:41:52 -0300

    Modificando vista de admin

--- challenge/bigbox/admin.py
+++ challenge/bigbox/admin.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Category)
+class ActivityAdmin(admin.ModelAdmin):
+   # fields = ['category', 'description'] 
+
+   fieldsets = [
+       (None,               {'fields': ['category','purchase_available']}),
+       ('Datos',            {'fields': ['description', 'name', 'internal_name']}),
+   ]
+   # exclude = ['internal_name']
+class BoxInline(admin.StackedInline):
+    model = Box
+    extra = 3
+
+class CategoryAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,              {'fields': ['name']}),
+        ('Datos',           {'fields': ['description','order','slug']}),
+    ]
+    inlines = [BoxInline]
+
+admin.site.register(Activity, ActivityAdmin)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Box)
-admin.site.register(Activity)
 admin.site.register(Reason)
